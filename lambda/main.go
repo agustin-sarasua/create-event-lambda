d32c8ac55676febb13f62d6d664c1c5a6451e377
Agustin Sarasua <agustin.sarasua@mercadolibre.com>
2018-08-09 20:14:03 -0300

    fix deploy

--- lambda/main.go
+++ lambda/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -10,16 +14,19 @@ type book struct {
 	Author string `json:"author"`
 }
 
-func show() (*book, error) {
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	bk := &book{
 		ISBN:   "978-1420931693",
 		Title:  "The Republic",
 		Author: "Plato",
 	}
-
-	return bk, nil
+	resp, err := json.Marshal(bk)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	return events.APIGatewayProxyResponse{Body: string(resp), StatusCode: 200, IsBase64Encoded: false}, nil
 }
 
 func main() {
-	lambda.Start(show)
+	lambda.Start(handleRequest)
 }
